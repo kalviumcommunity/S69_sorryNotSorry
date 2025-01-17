36daad4628bf74a5ff671928f09832d20b73a329
dhananjaiyadav1234 <66487160+dhananjaiyadav1234@users.noreply.github.com>
2025-01-17 12:42:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const express = require ('express');
+const app = express();
+
+
 app.get("/",(req,res)=>{
     res.send("Hello, welcome to my ASAP Project");
 })
@@ -6,6 +10,6 @@ app.get("/ping",(req,res)=>{
     res.send("Pong");
 })
 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
+app.listen(8000,()=>{
+    console.log("Server is running on port 8000");
 })
